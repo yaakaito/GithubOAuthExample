a14214a1d094e27d1df5be57f3cf4deb01264a19
KAZUMA Ukyo <yaakaito@gmail.com>
2012-09-01 17:22:59 +0900

    add key.h

---
+++ GithubOAuthExample/Keys.h
@@ -0,0 +1,14 @@
+//
+//  Keys.h
+//  GithubOAuthExample
+//
+//  Created by yaakaito on 12/09/01.
+//  Copyright (c) 2012年 yaakaito. All rights reserved.
+//
+
+#ifndef GithubOAuthExample_Keys_h
+#define GithubOAuthExample_Keys_h
+
+#define kClientId       (@"clientid")
+#define kClientSecret   (@"clientsecret")
+#endif
